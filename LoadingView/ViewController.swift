2718b8abee634aa2c90968c02df34d02c824817a
Tanya Landsman <tlandsman@activecampaign.com>
2019-05-11 17:38:38 -0500

    Initial Commit

---
+++ LoadingView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingView
+//
+//  Created by Tanya Landsman on 5/11/19.
+//  Copyright © 2019 Tanya Landsman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
